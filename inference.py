508c50883f390f4cf67a184d98600d27bc259bda
A_Nama <145112688+A-Nama@users.noreply.github.com>
2025-05-09 22:09:54 +0400

    model from hugging face

--- inference.py
+++ inference.py
@@ -1,12 +1,17 @@
 import torch
 import numpy as np
 from transformers import AutoTokenizer, AutoModelForSequenceClassification
+from dotenv import load_dotenv
+import os
 
-model_path = "model/bias_bert_model" 
+# Load HF token from .env
+load_dotenv()
+hf_token = os.getenv("HF_TOKEN")
+hf_model_id = "aishanama/Code-Purple"  
 
-# Load model and tokenizer
-tokenizer = AutoTokenizer.from_pretrained(model_path)
-model = AutoModelForSequenceClassification.from_pretrained(model_path)
+# Load model and tokenizer from Hugging Face using auth token
+tokenizer = AutoTokenizer.from_pretrained(hf_model_id, use_auth_token=hf_token)
+model = AutoModelForSequenceClassification.from_pretrained(hf_model_id, use_auth_token=hf_token)
 model.eval()
 
 def predict_bias(paragraphs, max_length=512):
@@ -14,4 +19,14 @@ def predict_bias(paragraphs, max_length=512):
     with torch.no_grad():
         outputs = model(**inputs)
         scores = torch.sigmoid(outputs.logits).squeeze().numpy()
-        return scores  # Between 0 and 1 now
+        return scores
+
+def interpret_bias_score(score):
+    if score < 0.4:
+        return "✅ Very Inclusive"
+    elif score < 0.6:
+        return "🟡 Fairly Neutral"
+    elif score < 0.8:
+        return "🟠 Likely Biased"
+    else:
+        return "🔴 Highly Biased / Exclusionary"
